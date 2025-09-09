98fea37a4f8fb936f5a16b34858aec6570bab237
espennoreng <espennoreng@gmail.com>
2025-09-09 20:29:10 +0200

    first

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,87 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+import { Text } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+// Text color: 1E1E1E
+// color not active: 71727A
+// active icon color: 006FFD
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs screenOptions={{}}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarLabel: ({ focused, color }) => (
+            <Text
+              style={{
+                color: focused ? "#1E1E1E" : color,
+                fontSize: 12,
+                fontWeight: focused ? "500" : "normal",
+                marginTop: 4,
+              }}
+            >
+              Home
+            </Text>
+          ),
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name="home"
+              color={focused ? "#006FFD" : color}
+              size={24}
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="members"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Members",
+          tabBarLabel: ({ focused, color }) => (
+            <Text
+              style={{
+                color: focused ? "#1E1E1E" : color,
+                fontSize: 12,
+                fontWeight: focused ? "500" : "normal",
+                marginTop: 4,
+              }}
+            >
+              Members
+            </Text>
+          ),
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name="people"
+              color={focused ? "#006FFD" : color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarLabel: ({ focused, color }) => (
+            <Text
+              style={{
+                color: focused ? "#1E1E1E" : color,
+                fontSize: 12,
+                fontWeight: focused ? "500" : "normal",
+                marginTop: 4,
+              }}
+            >
+              Profile
+            </Text>
+          ),
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name="person"
+              color={focused ? "#006FFD" : color}
+              size={24}
+            />
+          ),
         }}
       />
     </Tabs>
